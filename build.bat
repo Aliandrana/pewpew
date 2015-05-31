b802431925f0c23b418e0cae33dcd87514574322
Colin McMillen <c@16bit.cc>
2015-05-31 12:32:36 -0400

    Change output file extension .smc -> .sfc

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 pcx2snes -s32 -c16 -o16 -n sprites32
 pcx2snes -s16 -c4 -o4 -n tiles
 wla-65816 -o pewpew.asm pewpew.o
-wlalink -vr wlalink.cfg pewpew.smc
+wlalink -vr wlalink.cfg pewpew.sfc
 del *.o
